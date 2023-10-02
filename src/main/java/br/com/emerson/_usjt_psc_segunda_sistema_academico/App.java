d7c09ae24b90cd98c474b8e059ccdc60b25a8db7
Emerson Previatto <kubrrick@gmail.com>
2023-10-02 20:58:23 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/emerson/_usjt_psc_segunda_sistema_academico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.emerson._usjt_psc_segunda_sistema_academico;
+
+/**
+ *
+ * @author 823211664
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
